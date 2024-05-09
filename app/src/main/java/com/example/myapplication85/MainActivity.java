ce71b585b2e1c248e7179d635544a95686afa783
unknown <you@example.com>
2024-05-09 16:03:41 +0600

    textView

--- app/src/main/java/com/example/myapplication85/MainActivity.java
+++ app/src/main/java/com/example/myapplication85/MainActivity.java
@@ -3,17 +3,15 @@ package com.example.myapplication85;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
